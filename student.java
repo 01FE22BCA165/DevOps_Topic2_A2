0d5d41c9351bab085716f99dc8cd35efc34d0b0c
01FE22BCA165 <01fe22bca165@kletech.ac.in>
2024-02-07 16:41:28 +0530

    total marks

--- student.java
+++ student.java
@@ -25,5 +25,6 @@ public class student {
         s.displayName();
         s.displayAge("03-10-2003");
         s.displayStudentMarks();
+        s.totalStudentMarks();
 }
 }
--- studentcourseinfo.java
+++ studentcourseinfo.java
@@ -1,12 +1,35 @@
 public class studentcourseinfo extends student {
     private String[] courseCodes = { "C#", "ST", "ML", "DEVOPS", "MP" };
-    private int[] marks = { 85, 92, 75, 88, 95 };
+    private int[] marks_IA1 = { 85, 92, 75, 88, 95 };
+    private int[] marks_IA2 = { 85, 92, 75, 88, 95 };
+
 
     public void displayStudentMarks() {
-        System.out.println("Course Codes \t Marks Obtained");
+        System.out.println("Course Codes \t Marks Obtained in IA_1 \tIA_2 ");
 
         for (int i = 0; i < courseCodes.length; i++) {
-            System.out.println(courseCodes[i] + "\t\t\t" + marks[i]);
+            System.out.println(courseCodes[i] + "\t\t\t" + marks_IA1[i]+ "\t\t\t" + marks_IA2[i]);
         }
+       
     }
-}
+    /**
+     * @return
+     */
+    public void totalStudentMarks(){
+        int total=0;
+        int total_2=0;
+        for(int i = 0; i < marks_IA1.length; i++){
+            
+             total += marks_IA1[i];
+        }
+        System.out.println("Total marks in IA_1: "+total);
+    
+    for(int i = 0; i < marks_IA2.length; i++){
+        
+        total_2 += marks_IA2[i];
+   }
+   System.out.println("Total marks in IA_2: "+total_2);
+
+    int totalmarks= total+total_2;
+    System.out.println("Total marks"+totalmarks);
+}}
